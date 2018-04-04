2ecc92f4f02403905a6a2e5c2ad1d80efa170830
FuChai Mead <mead.fuchai@gmail.com>
2018-04-03 22:38:41 -0600

    fibonacci

--- ruby_challenges.rb
+++ ruby_challenges.rb
@@ -1,7 +1,8 @@
 #ruby challenges 
 require 'pry'
 
-def LongestWord(sen)
+#longest word in a sentence 
+def longest_word(sen)
   longest_word = sen.split(/[^\w]+/).max_by(&:length)
   puts "longest word is #{longest_word}" 
 end
@@ -9,5 +10,31 @@ end
 
 puts "enter a sentence"
 sen = gets.strip
-LongestWord(sen)
+longest_word(sen)
+
+
+#fibonacci
+#input is num 
+# if num = 1 we want output to be 1
+# if num = 2 we want output to be 2
+# if num > 2 we want the sum of the previous values
+def fibonacci(num)
+  a = 1
+  b = 0
+
+  while num >= 0 do
+    temp = a 
+    a = b
+    b = temp + b
+    num -= 1
+  end 
+  puts a 
+end  
+
+puts "enter a number"
+num = gets.strip.to_i
+fibonacci(num)
+
+
+
 
